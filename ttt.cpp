d39d862bb18d82a3c2efa4bbcf4eb41f1fce9038
Marco <patrickstar1281@yahoo.com>
2023-10-07 10:50:24 -0500

    added graph layout

--- ttt.cpp
+++ ttt.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello world!\n";
+    char space[3][3] = {{'1','2','3'}, {'4','5','6'}, {'7','8','9'}};
+
+    std::cout << "     |     |     \n";
+    std::cout << "   " << space[0][0] << " |  " << space[0][1] << "  |  " << space[0][2] << "   \n";
+    std::cout << "_____|_____|_____\n";
+    std::cout << "     |     |     \n";
+    std::cout << "   " << space[1][0] << " |  " << space[1][1] << "  |  " << space[1][2] << "   \n";
+    std::cout << "_____|_____|_____\n";
+    std::cout << "     |     |     \n";
+    std::cout << "   " << space[2][0] << " |  " << space[2][1] << "  |  " << space[2][2] << "   \n";
+    std::cout << "     |     |     \n";
 }
